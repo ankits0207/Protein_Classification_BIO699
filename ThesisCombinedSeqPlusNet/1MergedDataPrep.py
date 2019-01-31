9245de0a09801233c5a40d03341f708db9599de7
Ankit Sharma <ankitsharma0207@gmail.com>
2019-01-31 15:35:25 +0530

    Delete 1MergedDataPrep.py

--- ThesisCombinedSeqPlusNet/1MergedDataPrep.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-
-Sdf = pd.read_csv('SeqFV.csv')
-NDf = pd.read_csv('NetFV.csv')
-NDf['pdbId'] = NDf['pdbId'].str.upper()
-MergedDf = pd.merge(Sdf, NDf, how='inner', on=['pdbId', 'label'])
-MergedDf.to_csv('MergedData.csv', index=False)
-print('Done')
